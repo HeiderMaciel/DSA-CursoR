6b7cbc1606728a585b64c84012dc7bc3a00a4074
Heider <hrmaciel@gmail.com>
2018-07-19 08:06:51 -0300

    inicial

---
+++ Cap2/01-RStudio_Heider.R
@@ -0,0 +1,20 @@
+# instalando
+install.packages("randomForest")
+install.packages("ggplot2")
+
+# Carregando
+library(ggplot2)
+
+# Descarregando
+detach(ggplot2)
+
+#help
+help(mean)
+?mean
+
+# Se nao souber o nome da funcao
+help.search('randomForest')
+??matplot
+
+RSiteSearch('matplot')
+example('matplot')
